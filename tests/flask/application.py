74aeae040b79b4d90f2055a355779b5f17b47a4e
batikanor <57166099+batikanor@users.noreply.github.com>
2020-03-03 17:59:31 +0300

    tested db connections with sqlite3

--- tests/flask/application.py
+++ tests/flask/application.py
@@ -14,17 +14,25 @@ Error: Could not locate a Flask application. You did not provide the "FLASK_APP"
 export FLASK_APP=application.py
 """
 from flask import Flask, render_template
-
+import sqlite3 
 app = Flask(__name__)
+db = "batifinance.db"
 
 @app.route("/")
 def index():
+    
     return render_template("index.html")
 
 @app.route("/hello")
 def hi():
-    return "Hello, git!"
+    with sqlite3.connect(db) as con:
+        cur = con.cursor()
+        rows = cur.execute("SELECT * FROM users")
+        print(rows)
+        meh = rows.fetchall()
+        return str(meh)
 
-@app.route("/<string:name>")
+@app.route("/hello/<string:name>")
 def greet(name):
     return f"Hello, {name.capitalize()}"
+
