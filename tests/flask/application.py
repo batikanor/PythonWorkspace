24a0d02e79fc9f8ea9f339e510d179c9c8ab8a48
batikanor <57166099+batikanor@users.noreply.github.com>
2020-03-04 00:11:01 +0300

    tried google cloud sql connector

--- tests/flask/application.py
+++ tests/flask/application.py
@@ -5,7 +5,7 @@ __author__ = "BATIKAN BORA ORMANCI"
 """
 flask run
 """
-# If you get following error : 
+# If you get following error :
 """
 Error: Could not locate a Flask application. You did not provide the "FLASK_APP" environment variable, and a "wsgi.py" or "app.py" module was not found in the current directory.
 """
@@ -14,19 +14,27 @@ Error: Could not locate a Flask application. You did not provide the "FLASK_APP"
 export FLASK_APP=application.py
 """
 from flask import Flask, render_template
-import sqlite3 
+from google.cloud.sql.connector import connector
 app = Flask(__name__)
-db = "batifinance.db"
+
+con = connector.connect(
+    # PROJECT_ID:REGION:INSTANCE_ID
+    "pure-hue-270016:europe-west1:bati",
+    "pymysql",
+    user="bati",
+    password="iH0DvHFIjx69hn21",
+    database="finance"
+)
+
 
 @app.route("/")
 def index():
-    
+
     return render_template("index.html")
 
 @app.route("/hello")
 def hi():
-    with sqlite3.connect(db) as con:
-        cur = con.cursor()
+    with con.cursor() as cur:
         rows = cur.execute("SELECT * FROM users")
         print(rows)
         meh = rows.fetchall()
